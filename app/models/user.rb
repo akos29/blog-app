f4c0427d057c43696d4641461d1509b6a65973ae
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-07-27 12:04:26 +0300

    Add email confirmation for user registeration

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   attribute :posts_count, :integer, default: 0
   attribute :photo, :string, default: 'https://i.pravatar.cc/300'
   validates :name, presence: true, length: { minimum: 2 }
